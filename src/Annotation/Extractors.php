78489a8790ec8aae05d0b4c80249b2460f007795
Akihito Koriyama <akihito.koriyama@gmail.com>
2022-12-20 13:34:29 +0900

    Add attribute

--- src/Annotation/Extractors.php
+++ src/Annotation/Extractors.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Ray\Auth0Module\Annotation;
 
+use Attribute;
 use Doctrine\Common\Annotations\Annotation\Target;
 use Ray\Di\Di\Qualifier;
 
@@ -12,6 +13,7 @@ use Ray\Di\Di\Qualifier;
  * @Target("METHOD")
  * @Qualifier()
  */
+#[Attribute(Attribute::TARGET_METHOD), Qualifier]
 class Extractors
 {
 }
--- src/Provider/ManagementClientInject.php
+++ src/Provider/ManagementClientInject.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Ray\Auth0Module\Provider;
 
 use Auth0\SDK\API\Management;
+use Ray\Di\Di\Inject;
 
 trait ManagementClientInject
 {
@@ -16,6 +17,7 @@ trait ManagementClientInject
     /**
      * @\Ray\Di\Di\Inject
      */
+    #[Inject]
     public function setManagementClient(Management $managementClient) : void
     {
         $this->managementClient = $managementClient;
